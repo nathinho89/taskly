3c31378acffda4c895674af9d74db41e3a1a0c2f
nathan-richards <nathinho@outlook.com>
2024-08-21 16:18:40 +0100

    Added no-unused styles plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ['expo', 'prettier'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'react-native'],
   rules: {
     'prettier/prettier': [
       'error',
@@ -12,5 +12,6 @@ module.exports = {
         semi: true,
       },
     ],
+    'react-native/no-unused-styles': 'error',
   },
 };
