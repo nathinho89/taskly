5074d6c92ba17c898eb7f08be9c8ce8f21e551f9
nathan-richards <nathinho@outlook.com>
2024-08-21 14:26:54 +0100

    Shopping list item created

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { theme } from './theme';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemContainerText}>Coffee</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: theme.colorWhite,
     justifyContent: 'center',
+  },
+  itemContainer: {
+    borderBottomColor: theme.colorCerulian,
+    borderBottomWidth: 1,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+  },
+  itemContainerText: {
+    fontSize: 18,
+    fontWeight: '200',
   },
 });
---
+++ theme.ts
@@ -0,0 +1,4 @@
+export const theme = {
+  colorCerulian: '#1a759f',
+  colorWhite: '#fff',
+};
