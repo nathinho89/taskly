d9731e5910541c747faf8aea3d0d868c3f3b1836
nathan-richards <nathinho@outlook.com>
2024-08-21 15:24:41 +0100

    Delete modal and theming

--- App.tsx
+++ App.tsx
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 import { theme } from './theme';
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert(
+      'Are you sure you want to delete this?',
+      'It will be gone for good',
+      [
+        {
+          text: 'Yes',
+          onPress: () => console.log('Ok, deleting'),
+          style: 'destructive',
+        },
+        { text: 'Cancel', style: 'cancel' },
+      ],
+    );
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
         <Text style={styles.itemContainerText}>Coffee</Text>
+        <TouchableOpacity
+          style={styles.itemContainerBtn}
+          onPress={handleDelete}
+          activeOpacity={0.8}
+        >
+          <Text style={styles.itemContainerBtnText}>Delete</Text>
+        </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -24,9 +46,23 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     paddingHorizontal: 8,
     paddingVertical: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   itemContainerText: {
     fontSize: 18,
     fontWeight: '200',
+  },
+  itemContainerBtn: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  itemContainerBtnText: {
+    color: theme.colorWhite,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    letterSpacing: 1,
   },
 });
--- theme.ts
+++ theme.ts
@@ -1,4 +1,5 @@
 export const theme = {
   colorCerulian: '#1a759f',
   colorWhite: '#fff',
+  colorBlack: '#000',
 };
