a01f5a34f16679cd0704d3d93fb39ecf35774965
nathan-richards <nathinho@outlook.com>
2024-08-27 22:20:12 +0100

    Add a text input for shopping list

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TextInput } from 'react-native';
+import React, { useState } from 'react';
 import { theme } from '../theme';
 import { ShoppingListItem } from '../components/ShoppingListItem';
 
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: '1', name: 'Coffee' },
+  { id: '2', name: 'Tea' },
+  { id: '3', name: 'Sugar' },
+];
+
 export default function App() {
+  const [list, setList] = useState(initialList);
+  const [value, setValue] = useState('');
+
+  const handleSubmit = () => {
+    if (value) {
+      const newList = [
+        {
+          id: new Date().toISOString(),
+          name: value,
+        },
+        ...list,
+      ];
+      setList(newList);
+      setValue('');
+    }
+  };
+
   return (
     <View style={styles.home}>
-      <ShoppingListItem name="Coffee" isCompleted />
-      <ShoppingListItem name="Tea" />
-      <ShoppingListItem name="Sugar" isCompleted />
+      <TextInput
+        style={styles.homeTextInput}
+        placeholder="E.g. Coffee"
+        value={value}
+        onChangeText={setValue}
+        keyboardType="default"
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+      {list.map((item) => (
+        <ShoppingListItem key={item.id} name={item.name} />
+      ))}
       <StatusBar style="auto" />
     </View>
   );
@@ -18,6 +56,14 @@ const styles = StyleSheet.create({
   home: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: 'center',
+  },
+  homeTextInput: {
+    borderColor: theme.colorLightGray,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginVertical: 12,
+    fontSize: 18,
+    borderRadius: 50,
   },
 });
